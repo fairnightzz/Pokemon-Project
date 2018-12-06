af308f44f1b93151b21f198316cd156579f8f048
Mark Zhehai Zhang <zhehaizhang1@gmail.com>
2018-12-05 23:27:55 -0500

    Added new files

--- src/LoadPokemon.java
+++ src/LoadPokemon.java
@@ -1,8 +1,11 @@
 import java.io.*;
 import java.util.*;
 public class LoadPokemon {
-    private static String deck[] = new String[4];
+    //private static String deck[] = new String[4];
+    private static ArrayList<ArrayList<String>> pdeck = new ArrayList();
     private static ArrayList<ArrayList<String>> pokemons = new ArrayList<ArrayList<String>>();
+    private static ArrayList<ArrayList<String>> bdeck = new ArrayList();
+    private static final ArrayList<String>empty = new ArrayList();
     public static boolean load(){
         try{
             Scanner inFile = new Scanner(new BufferedReader(new FileReader("pokemon.txt")));
@@ -13,8 +16,13 @@ public class LoadPokemon {
                 ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(p));
                 pokemons.add(arrayList);
             }
-
             //	<name>,<hp>,<type>,<resistance>,<weakness>,<num attacks>,[<attack name>, <energycost>,<damage>,<special>
+
+            empty.add("Empty");
+            for (int i = 0;i<4;i++){
+                pdeck.add(empty);
+                bdeck.add(empty);
+            }
             return true;
         }
         catch(IOException ex){
@@ -33,33 +41,49 @@ public class LoadPokemon {
         invalid.add("Invalid");
         return invalid;
     }
+    public static ArrayList<ArrayList<String>> bChoose(ArrayList<ArrayList<String>> pokemon){
+        for (int i = 0;i<4;i++){
+            int choice = (int)Math.random()*pokemons.size();
+            bdeck.set(i,pokemons.get(choice));
+            pokemons.remove(choice);
+        }
+        return pokemon;
+    }
 
-    public static String[] choose(String[] pokemon){
+    public static boolean pCheck(){
+        if (pdeck.contains(empty)){
+            return false;
+        }
+        return true;
+    }
+
+    public static ArrayList<ArrayList<String>> choose(ArrayList<ArrayList<String>> pokemon){
         Scanner kb = new Scanner(System.in);
         int option = 0;
         int selected = 0;
-        deck = new String[4];
+        //pdeck = new ArrayList();
         while (option!=-1){
             display(1);
             display(2);
             option = kb.nextInt();
             if (option-1>=0 && option-1<=pokemons.size()){
-                deck[selected] = pokemons.get(option-1).get(0);
+                pdeck.set(selected,pokemons.get(option-1));
+                pokemons.remove(option-1);
                 selected+=1;
                 if (selected == 4){
                     display(2);
-                    return deck;
+                    return pdeck;
                 }
-
             }
-            else if (option!=-1){
+            else{
                 System.out.println("Invalid Selection, try again.");
+                option = 0;
             }
         }
+
         return pokemon;
     }
-
-    private static void display(int choice){
+    public static void display(int choice){
         if (choice == 1){
             String display[][] = new String[(pokemons.size()+1)][1];
             display[0][0] = "Enter a number to choose your Pokemon";
@@ -73,13 +97,10 @@ public class LoadPokemon {
         else if (choice == 2){
             String display[][] = new String[5][1];
             display[0][0] = "Current Deck:";
-            for (int i = 1; i<deck.length+1;i++){
-                if (deck[i-1] == null){
-                    display[i][0] = "Empty";
-                }
-                else{
-                    display[i][0] = deck[i-1];
-                }
+            for (int i = 1; i<5;i++){
+
+                display[i][0] = pdeck.get(i-1).get(0);
+
 
             }
             PokemonArena.pprint(display);
--- src/PokemonArena.java
+++ src/PokemonArena.java
@@ -11,7 +11,8 @@ public class PokemonArena {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-    public static String[] deck = new String[4];
+    public static ArrayList<ArrayList<String>> deck = new ArrayList();
+    public static ArrayList<ArrayList<String>> botdeck = new ArrayList();
     public static void main(String[] args) {
         System.out.println("Pokemon Project");
         if (LoadPokemon.load()!= true){
@@ -26,30 +27,54 @@ public class PokemonArena {
         System.out.println("Welcome to the Pokemon Project.");
         Scanner kb = new Scanner(System.in);
         int option = 0;
-        while (option==0){
+        while (option!=-1){
             pprint(new String [][]{{"Main Menu"},{"Battle"},{"Choose Deck"},{"View Pokemon"}});
             option = kb.nextInt();
             switch (option){
                 case 1://Battle
                     //Run the function
                     System.out.println("Battle!");
-
+                    if (LoadPokemon.pCheck() == false){
+                        System.out.println("Please select your deck first!");
+                        break;
+                    }
+                    botdeck = LoadPokemon.bChoose(botdeck);
+                    returnM();
                     break;
                 case 2://Choose Deck
                     //Choose the Deck
                     System.out.println("Choose your deck!");
-
-
-                    LoadPokemon.choose(deck);
-
+                    deck = LoadPokemon.choose(deck);
+                    returnM();
                     break;
+                case 3://View Deck
+                    System.out.println("View your deck");
+                    LoadPokemon.display(2);
+                    returnM();
+                    break;
+
+                case -1://break
+                    System.out.println("Game exited.");
+                    break;
+
                 default:
                     System.out.println("Invalid Response, try again");
-                    option = 0;
 
             }
         }
     }
+
+    private static void returnM(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println(ANSI_RED+"Return to main menu? (-1)"+ANSI_RESET);
+        int exit = 0;
+        while (exit !=-1){
+            exit = kb.nextInt();
+        }
+
+
+    }
+
     public static String pprint(String[][] parameters){
         String print = "";
         for (int i = 0;i<parameters.length;i++){
@@ -74,7 +99,5 @@ public class PokemonArena {
         return print;
     }
 
-    private void choose(){
 
-    }
 }
